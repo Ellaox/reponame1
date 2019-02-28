796c4fc78142082eb814805587c11cac51752a2f
Ellaox <40073721+Ellaox@users.noreply.github.com>
2019-02-28 12:49:45 +0000

    average2.py

---
+++ workshop/week4/average2.py
@@ -0,0 +1,38 @@
+#average.py
+#A program that prompts the user to enter two numbers
+#adds them and averages them, then outputs the results to the screen
+#P.K, 5TH  December 2004
+
+print"This program will ask you to type in two numbers."
+print "The program will add them & average them and"
+print"output the results to the screen"
+print
+num1 = input("Type in a number: ")
+print
+num2 = input("Type in another number: ")
+print
+sum = num1 + num2
+print "The sum of the numbers is", sum
+average = sum/2.0
+print "Their average is", average
+
+
+#Workshop results
+
+#average.py
+#A program that prompts the user to enter two numbers
+#adds them and averages them, then outputs the results to the screen
+#P.K, 5TH  December 2004
+
+print"This program will ask you to type in two numbers."
+print "The program will add them & average them and"
+print"output the results to the screen"
+print
+num1 = input("Type in a number: ")
+print
+num2 = input("Type in another number: ")
+print
+sum = num1 + num2
+print "The sum of the numbers is", sum
+average = sum/2.0
+print "Their average is", average
